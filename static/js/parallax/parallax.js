f714bf17e5f12d76dcbf66af6a71761efcc90582
Kieran Sweeden <kieransweeden@gmail.com>
2022-03-09 23:24:02 +0000

    Add placeholder design for welcome page

--- static/js/parallax/parallax.js
+++ static/js/parallax/parallax.js
@@ -1,4 +1,21 @@
 window.addEventListener('DOMContentLoaded', () => {
-    let scene = document.getElementById('scene');
-    let parallaxInstance = new Parallax(scene);
+
+    // Grab page header
+    let pageHeader = [...document.getElementsByTagName('h2')][0].innerText;
+
+    switch(pageHeader) {
+        case "Welcome":
+            let images = document.querySelectorAll('img');
+            new simpleParallax(images, {
+                delay: .6,
+	            transition: 'cubic-bezier(0,0,0,1)'
+            });
+            break;
+        case "Log in":
+        case "Register":
+            let scene = document.getElementById('scene');
+            let parallaxInstance = new Parallax(scene);
+            break;
+      }
 });
+
