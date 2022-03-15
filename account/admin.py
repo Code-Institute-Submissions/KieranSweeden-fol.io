5a3809c0af7854aee5b96e3a701d06ae50dc2a75
Kieran Sweeden <kieransweeden@gmail.com>
2022-03-15 19:00:26 +0000

    Register user account to admin panel

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,11 @@
+"""
+This file registers models created within this application
+to the Django admin panel
+"""
+
 from django.contrib import admin
+from .models import UserAccount
 
 # Register your models here.
+
+admin.site.register(UserAccount)
