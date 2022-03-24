e42dea2fec2e1042c54c4a3f5d02cc616cbec8b4
Kieran Sweeden <kieransweeden@gmail.com>
2022-03-24 23:00:00 +0000

    Add open folio in suite functionality

--- suite/urls.py
+++ suite/urls.py
@@ -6,8 +6,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('open/<int:folio_id>/', views.open_suite, name="open_suite"),
     path('select/', views.select_folio, name="select_folio"),
-    path('projects/', views.view_folio_projects, name="view_folio_projects"),
+    path('projects/<int:folio_id>/',
+         views.view_folio_projects,
+         name="view_folio_projects"),
     path('skills/', views.view_folio_skills, name="view_folio_skills"),
     path('profile/', views.view_folio_profile, name="view_folio_profile"),
     path('contact/', views.view_folio_contact, name="view_folio_contact"),
--- suite/views.py
+++ suite/views.py
@@ -2,8 +2,31 @@
 Views for the pages related to the folio suite
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def open_suite(request, folio_id):
+    """
+    This view attempts to direct the user to
+    the suite. If a folio id has not been provided,
+    the user will be directed to the select folio view.
+    Else they'll be directed to the suite viewing folio.
+    """
+
+    # Assess whether folio_id has been provided
+    if folio_id:
+        # If it has, direct the user to folio view
+        # attaching the folio id as an argument
+        # return redirect('view_folio_projects', folio_id=folio_id)
+        return redirect(reverse("view_folio_projects",
+                                kwargs={"folio_id": folio_id}))
+
+    else:
+        # If one hasn't  been provided
+        return redirect("select_folio")
+
 
 @login_required
 def select_folio(request):
@@ -17,7 +40,7 @@ def select_folio(request):
 
 
 @login_required
-def view_folio_projects(request):
+def view_folio_projects(request, folio_id):
     """
     Presents the projects tab of the
     folio to the user.
