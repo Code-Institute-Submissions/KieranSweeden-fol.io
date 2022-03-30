58e29ff349057e378956711fbe60cac7ad6cca78
Kieran Sweeden <kieransweeden@gmail.com>
2022-03-30 21:48:12 +0000

    Add listener for checkboxes in folio project screen

--- static/js/suite/folio_projects.js
+++ static/js/suite/folio_projects.js
@@ -1,9 +1,25 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+
+    // Ensure checkboxes can be checked & unchecked
+    setupCheckBoxClickListeners();
     
-    // Setup the abiltiy to update projects
+    // Setup the abilty to update projects
     setupUpdateProjectsInFolioFunctionality();
 
 });
+
+function setupCheckBoxClickListeners(){
+
+    // Get all project checkboxes on page
+    const projectCheckBoxes = [...document.getElementsByClassName('form-check-input')];
+
+    // Apply click listener to checkboxes
+    projectCheckBoxes.forEach(projectCheckBox => {
+        projectCheckBox.addEventListener('change', (event) => {
+            event.target.toggleAttribute("checked");
+        });
+    });
+}
 
 function setupUpdateProjectsInFolioFunctionality(){
     // Grab save button
@@ -18,20 +34,26 @@ function updateProjectsInFolio(){
     // Grab folio from URL & project ID's from checkboxes
     const folioID = window.location.pathname.split('/')[3];
     const projectCheckBoxes = [...document.getElementsByClassName('form-check-input')];
-    const projectIDs = projectCheckBoxes.map(projectCheckBox => projectCheckBox.id);
+    const projectsAttachedIDs = projectCheckBoxes.filter(returnProjectIDIfChecked);
+
+    console.log(projectsAttachedIDs)
 
     // Create AJAX Request & csrf token
-    let request = new XMLHttpRequest();
-    const CSRF_TOKEN = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
+    // let request = new XMLHttpRequest();
+    // const CSRF_TOKEN = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
 
-    request.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            location.reload();
-        }
-    }
+    // request.onreadystatechange = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         location.reload();
+    //     }
+    // }
 
-    request.open("POST", `/suite/projects/update/projects_attached/${folioID}/`, true);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    request.setRequestHeader("X-CSRFToken", CSRF_TOKEN);
-    request.send(`{"projects":${JSON.stringify(projectIDs)}}`);
+    // request.open("POST", `/suite/projects/update/projects_attached/${folioID}/`, true);
+    // request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    // request.setRequestHeader("X-CSRFToken", CSRF_TOKEN);
+    // request.send(`{"projects":${JSON.stringify(projectIDs)}}`);
+}
+
+function returnProjectIDIfChecked(projectCheckBox){
+    return projectCheckBox.hasAttribute("checked") ? projectCheckBox.id : false;
 }
