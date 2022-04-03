5cccee71e84715a86e191f8acac0806d7a3fe6ae
Kieran Sweeden <kieransweeden@gmail.com>
2022-04-03 18:10:36 +0000

    Add ability to create profiles

--- suite/urls/profile_urls.py
+++ suite/urls/profile_urls.py
@@ -6,10 +6,13 @@ from django.urls import path
 from suite import views
 
 profile_urlpatterns = [
-    path('profile/',
-         views.edit_folio_profile,
-         name="edit_folio_profile"),
-    path('profile/<int:folio_id>/',
-         views.edit_folio_profile,
-         name="edit_folio_profile")
+     path('profile/',
+          views.edit_folio_profile,
+          name="edit_folio_profile"),
+     path('profile/<int:folio_id>/',
+          views.edit_folio_profile,
+          name="edit_folio_profile"),
+     path('profile/create/<int:folio_id>/',
+          views.create_folio_profile,
+          name="create_folio_profile"),
 ]
--- suite/views/profile.py
+++ suite/views/profile.py
@@ -5,7 +5,8 @@ Views relating to the profile section of folios within suite
 from django.shortcuts import (
     render,
     redirect,
-    get_object_or_404
+    get_object_or_404,
+    reverse
 )
 from django.contrib.auth.decorators import login_required
 from suite.models import Folio, Profile
@@ -51,3 +52,35 @@ def edit_folio_profile(request, folio_id=None):
     else:
         # If one hasn't been provided
         return redirect("select_folio")
+
+
+@login_required
+def create_folio_profile(request, folio_id):
+    """
+    Creates a new folio profile
+    """
+
+    # Ensure the request made is a POST request
+    if request.method == "POST":
+
+        # Create instance of profile form using form data
+        form = FolioProfileForm(request.POST)
+
+        # If form is valid
+        if form.is_valid():
+
+            # Partially save the form, as author_id
+            # will need to be provided
+            profile = form.save(commit=False)
+
+            # Save current user as author of profile
+            profile.author_id = request.user
+
+            # Fully save profile
+            profile.save()
+
+            # Return to folio profile page using folio id
+            return redirect(
+                reverse("edit_folio_profile",
+                        kwargs={"folio_id": folio_id})
+            )
