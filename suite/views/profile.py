e2c7102e582f2ae8f15fb5bbe3c8a01614198fbe
Kieran Sweeden <kieransweeden@gmail.com>
2022-04-03 17:58:11 +0000

    Add insertion of users profiles to suite profile page

--- suite/views/profile.py
+++ suite/views/profile.py
@@ -8,7 +8,7 @@ from django.shortcuts import (
     get_object_or_404
 )
 from django.contrib.auth.decorators import login_required
-from suite.models import Folio
+from suite.models import Folio, Profile
 from suite.functions import id_has_been_provided
 from suite.forms import FolioProfileForm
 
@@ -24,11 +24,26 @@ def edit_folio_profile(request, folio_id=None):
 
         folio = get_object_or_404(Folio, pk=folio_id)
 
-        form = FolioProfileForm()
+        # Get the user's profiles
+        profiles = list(Profile.objects.filter(
+            author_id=request.user
+        ))
+
+        # For each profile, attach a form to the object
+        # And assess whether profile is attached to form
+        for profile in profiles:
+            profile.form = FolioProfileForm(instance=profile)
+
+            # Set is_attached to true if folio exists
+            # in the profiles list of folios
+            profile.is_attached = profile.folios.filter(pk=folio_id).exists()
+
+        create_profile_form = FolioProfileForm()
 
         context = {
             "folio": folio,
-            "form": form
+            "create_profile_form": create_profile_form,
+            "profiles": profiles
         }
 
         return render(request, "suite/edit_profile.html", context=context)
