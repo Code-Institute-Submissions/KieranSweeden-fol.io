01fdb2361fce9dcd513d6c5ed9f43b8a83cfdf6c
Kieran Sweeden <kieransweeden@gmail.com>
2022-04-26 17:04:41 +0000

    Add warning for attempting to publish a folio whilst not having enough licenses

--- library/views.py
+++ library/views.py
@@ -145,103 +145,82 @@ def update_folio(request, folio_id):
                 return response
 
     else:
-
         form = CreateFolioForm(instance=folio_in_db)
-
         context = {
             "form": form,
             "folio": folio_in_db
         }
-
         return render(request, "library/update_folio.html", context=context)
 
 
 @login_required
 def toggle_folio_published_state(request, folio_id):
     """
-    Updates the folio's published state
+    If folio is already published, it is toggled to false.
+    If not, depending on the amount of licenses the user holds,
+    either publish the selected folio or direct user to
+    purchase more licenses.
     """
 
-    # Get the folio
     folio = get_object_or_404(Folio, pk=folio_id)
 
-    # If folio's published state is true
     if folio.is_published:
-
-        # Set the folio's published state to false
         folio.toggle_published_state()
-
         messages.success(
             request,
             f"{folio.name} has been concealed successfully."
         )
-
-        # Return to library
         return redirect("view_library")
 
-    # If the folio's published state is false
     else:
-
-        # Get the user's amount of licences
         user_account = get_object_or_404(
             UserAccount, pk=request.user.id
         )
 
-        # Get the amount of folio's that are published
         amount_of_published_folios = len(Folio.objects.filter(
             author_id=request.user
         ).filter(
             is_published=True
         ))
 
-        # If amount of published folio's is less than user's amount of licences
         if amount_of_published_folios < user_account.number_of_licenses:
-
-            # Update the folio's published state to true
             folio.toggle_published_state()
-
             messages.success(
                 request,
                 f"{folio.name} has been published successfully."
             )
-
-            # Redirect user to library page with message
             return redirect("view_library")
-        # If amount of folio's published is >= than the user's amount of licenses
+
         else:
-            # Redirect to license purchase page with message
+            messages.warning(
+                request,
+                "You have used all of your licenses. "
+                "Please purchase additional licenses to publish more folios."
+            )
             return redirect("purchase_license")
 
 
 @login_required
 def delete_folio(request, folio_id):
     """
-    Deletes a user's folio when called
+    Deletes a user's folio when called &
+    removes it from the latest folio cookie
+    if it's the last folio visited
     """
 
-    # Get the folio using the folio id provided
     folio = get_object_or_404(Folio, pk=folio_id)
-
-    # Delete the folio
     folio.delete()
 
-    # Create redirect response
     response = redirect(reverse("view_library"))
+
+    if 'latest_folio' in request.COOKIES.keys():
+        latest_folio = request.COOKIES['latest_folio']
+        if int(folio_id) == int(latest_folio):
+            response.delete_cookie('latest_folio')
 
     messages.success(
         request,
         f"{folio.name} has been deleted successfully."
     )
 
-    # If the latest_folio cookie exists
-    if 'latest_folio' in request.COOKIES.keys():
-
-        # Get the cookie
-        latest_folio = request.COOKIES['latest_folio']
-
-        # Delete cookie if it's the id of deleted folio
-        if int(folio_id) == int(latest_folio):
-            response.delete_cookie('latest_folio')
-
-    # Re-direct user to library
     return response
