00fd254226391d371da18351da3c4215d28b5db4
Kieran Sweeden <kieransweeden@gmail.com>
2022-05-04 09:02:56 +0000

    Revert back to debug = true

--- folio/settings.py
+++ folio/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e0+8kwv(u8jk5fv=ok0@p=+l=xk5!87-q2-(v%l-rr)7^xxo(&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'folio-web-app.herokuapp.com',
