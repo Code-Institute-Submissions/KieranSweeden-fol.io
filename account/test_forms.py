7e16d84932463a58e6784910904c454dea0cc3c0
Kieran Sweeden <kieransweeden@gmail.com>
2022-05-04 12:08:33 +0000

    Add account details form unit testing within test_forms.py file

--- account/test_forms.py
+++ account/test_forms.py
@@ -2,18 +2,40 @@
 
 from django.test import TestCase
 
-from django.contrib.auth.models import User
 from .forms import AccountDetailsForm
 
 
-class TestForms(TestCase):
+class TestAccountDetailsForm(TestCase):
     """ Testing for forms within account app """
 
-    def setUp(self):
-        """ Creates records for testing purposes """
+    def test_account_fields_only_displayed(self):
+        """ Test only account detail fields are displayed """
+        form = AccountDetailsForm({})
+        self.assertEqual(form.Meta.fields, [
+            'profile_picture',
+            'first_name',
+            'last_name',
+            'phone_number',
+            'github_profile',
+            'linkedin_profile',
+        ])
 
-        self.user = User.objects.create(
-            email="test_user@testing.com",
-            username="test_user",
-            password="password_123",
+    def test_github_field_accept_urls_only(self):
+        form = AccountDetailsForm({
+            'github_profile': 'test'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('github_profile', form.errors.keys())
+        self.assertEqual(
+            form.errors['github_profile'][0], 'Enter a valid URL.'
+        )
+
+    def test_linkedin_field_accept_urls_only(self):
+        form = AccountDetailsForm({
+            'linkedin_profile': 'test'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('linkedin_profile', form.errors.keys())
+        self.assertEqual(
+            form.errors['linkedin_profile'][0], 'Enter a valid URL.'
         )
