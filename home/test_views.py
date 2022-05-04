f11d1050af44718f1246d5a36f53f822f8041043
Kieran Sweeden <kieransweeden@gmail.com>
2022-05-04 07:47:20 +0000

    Add test_views for home app

--- home/test_views.py
+++ home/test_views.py
@@ -1,12 +1,15 @@
-"""Test Home App Views"""
+"""Test home app views"""
 
 from django.test import TestCase
 
 
 class TestViews(TestCase):
     """
-    Test Home App Views
+    Test home app views
     """
 
     def test_home_view(self):
-        """"""
+        """Tests the home app view rendered correctly"""
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
