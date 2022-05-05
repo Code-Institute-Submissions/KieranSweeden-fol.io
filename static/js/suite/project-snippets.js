6dd8df42b2e24bf47f4d321b95a61ccffc6660fa
Kieran Sweeden <kieransweeden@gmail.com>
2022-05-05 20:05:11 +0000

    Add comments to project-snippets.js

--- static/js/suite/project-snippets.js
+++ static/js/suite/project-snippets.js
@@ -1,22 +1,22 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-
-    // Ensure checkboxes can be checked & unchecked
+/**
+ * JavaScript file that is used for interactivity
+ * regarding the project snippets found within
+ * the suite application
+ */
+window.addEventListener('DOMContentLoaded', () => {
     setupCheckBoxClickListeners();
-
-    // Assess amount of checkboxes checked
     assessTheAmountOfProjectCheckboxesChecked();
-    
-    // Setup the abilty to update projects
     setupUpdateProjectsInFolioFunctionality();
-
 });
 
+/**
+ * Provided checkboxes are present on page,
+ * add a change listener for each checkbox
+ * that toggles it's checked status and
+ * updates the displayed amount of checked projects
+ */
 function setupCheckBoxClickListeners(){
-
-    // Get all checkboxes on page
     const projectCheckBoxes = [...document.getElementsByClassName('form-check-input')];
-
-    // Apply click listener to checkboxes
     projectCheckBoxes.forEach(projectCheckBox => {
         projectCheckBox.addEventListener('change', (event) => {
             event.target.toggleAttribute("checked");
@@ -25,14 +25,16 @@ function setupCheckBoxClickListeners(){
     });
 }
 
+/**
+ * Makes sure that no more than 4 projects
+ * are checked at one time
+ */
 function assessTheAmountOfProjectCheckboxesChecked(){
-    // Init max amount of projects & grab checkboxes on page
     const MAX_AMOUNT_OF_PROJECTS_ALLOWED = 4;
     const projectCheckBoxes = [...document.getElementsByClassName('form-check-input')];
     const checkedProjectCheckboxes = projectCheckBoxes.filter(checkbox => checkbox.hasAttribute('checked'));
     const uncheckedCheckBoxes = projectCheckBoxes.filter(checkbox => !checkbox.hasAttribute('checked'));
 
-    // Update number of projects selected on page
     updateNumberOfProjectsSelected(checkedProjectCheckboxes.length);
 
     if (checkedProjectCheckboxes.length >= MAX_AMOUNT_OF_PROJECTS_ALLOWED){
@@ -41,73 +43,90 @@ function assessTheAmountOfProjectCheckboxesChecked(){
         const disabledCheckboxes = projectCheckBoxes.filter(checkbox => checkbox.hasAttribute('disabled'));
         enableDisabledProjectCheckboxes(disabledCheckboxes);
     }
-
 }
 
+/**
+ * Updates the element that displays how many projects
+ * are selected and defaults to 0 if none are checked
+ * @param {number} checkedProjectsAmount 
+ */
 function updateNumberOfProjectsSelected(checkedProjectsAmount = 0){
-    // Grab element on page
     const elementDisplayed = document.getElementById('selectedNumberOfProjects');
-
-    // Update value
     elementDisplayed.textContent = checkedProjectsAmount;
 }
 
+/**
+ * Disables each checkbox within
+ * the list that's send to it
+ * @param {[HTMLInputElement]} uncheckedCheckBoxes 
+ */
 function disableUncheckedProjectCheckboxes(uncheckedCheckBoxes){
-    for (checkbox of uncheckedCheckBoxes) {
+    for (let checkbox of uncheckedCheckBoxes) {
         checkbox.disabled = true;
     }
 }
 
+/**
+ * Enables each checkbox within
+ * the list that's send to it
+ * @param {[HTMLInputElement]} disabledCheckboxes 
+ */
 function enableDisabledProjectCheckboxes(disabledCheckboxes){
-    for (checkbox of disabledCheckboxes) {
+    for (let checkbox of disabledCheckboxes) {
         checkbox.disabled = false;
     }
 }
 
+/**
+ * Adds an event listener to the
+ * save changes button on the page
+ */
 function setupUpdateProjectsInFolioFunctionality(){
-    // Grab save button
     const saveChangesButton = document.getElementById("updateProjectsInFolioBtn");
-
-    // Apply event listener
     saveChangesButton.addEventListener('click', updateProjectsInFolio);
 }
 
+/**
+ * Sends an AJAX request to update the projects
+ * attached to the folio viewed on page by grabbing
+ * all checkboxes that represent projects and extracts
+ * project related data from them
+ */
 function updateProjectsInFolio(){
-    
-    // Grab folio from URL & project ID's from checkboxes
     const folioID = window.location.pathname.split('/')[3];
     const checkboxes = [...document.getElementsByClassName('form-check-input')];
 
-    // Filters down to project checkboxes only
     const projectCheckboxes = checkboxes.filter(checkbox => !checkbox.id.includes("image-clear_id"));
 
-    // Create a list of project objects containing their IDs & a status on if they're attached
     let listOfProjects = [];
     for (let projectCheckBox of projectCheckboxes){
-        listOfProjects.push(returnProjectObject(projectCheckBox))
+        listOfProjects.push(returnProjectObject(projectCheckBox));
     }
 
-    // Create AJAX Request & csrf token
     let request = new XMLHttpRequest();
     const CSRF_TOKEN = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
 
-    // Attach function for when request has been successfully made
     request.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             location.reload();
         }
-    }
+    };
 
-    // Prepare request to be sent with headers & send
     request.open("POST", `/suite/projects/update/projects_attached/${folioID}/`, true);
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     request.setRequestHeader("X-CSRFToken", CSRF_TOKEN);
     request.send(`{"projects":${JSON.stringify(listOfProjects)}}`);
 }
 
+/**
+ * Extracts and returns project related data
+ * from a checkbox sent to it
+ * @param {HTMLInputElement} projectCheckbox 
+ * @returns object containing id and is_attached status
+ */
 function returnProjectObject(projectCheckbox){
     return {
         id: projectCheckbox.id,
         is_attached: projectCheckbox.hasAttribute("checked")
-    }
+    };
 }
